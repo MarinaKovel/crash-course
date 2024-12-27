f0af6020581f3e4f480f8658b5b0e5ed2bcb8654
Marina Kovel <m.kovel@softteco.com>
2024-12-27 09:30:10 +0300

    feat: add product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
 
 
 @NgModule({
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ProductComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+//import { IProduct } from '../../models/product';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    imports: [CommonModule],
+})
+
+export class ProductComponent {
+    //@Input() product: IProduct
+
+    //details = false;
+}
