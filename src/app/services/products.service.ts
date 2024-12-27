16811ae8f0720f2344598feeef9f701a9e75a3e0
Marina Kovel <m.kovel@softteco.com>
2024-12-27 10:08:55 +0300

    feat: add built-in pipe delay for product service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { IProduct } from "../models/product";
-import { Observable } from "rxjs";
+import { delay, Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -19,6 +19,8 @@ export class ProductsService {
             // params: new HttpParams().append('limit', 5)
             // params: new HttpParams({ fromString: 'limit=5' })
             params: new HttpParams({ fromObject: {limit: 5} })
-        })
+        }).pipe(
+            delay(200)
+        )
     }
 }
